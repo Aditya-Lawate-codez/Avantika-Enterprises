d8325803ef6ad6e47b39c5c69b33dea1f7ef62ff
Aditya Lawate <149086756+Aditya-Lawate-codez@users.noreply.github.com>
2023-11-20 12:31:23 +0000

    removed favicon, modified index to use bootstrap

--- avantikaenterprises/src/App.js
+++ avantikaenterprises/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+
 
 function App() {
   return (
-   <>
-    <div>Avantika Enterprises</div>
-   </>
+    <>
+
+    </>
   );
 }
 
